4a517f76f4f51ce0d37d2226c57fff75e92c993e
Spencer Cawley <Spent_sir@Spencers-MacBook-Pro-2.local>
2018-08-10 02:05:48 -0600

    Added UIRefreshControl on home page & auto reload when post is shared

--- InstagramFIrebase/InstagramFIrebase/AddPhoto/SharePhotoController.swift
+++ InstagramFIrebase/InstagramFIrebase/AddPhoto/SharePhotoController.swift
@@ -11,6 +11,7 @@ import Firebase
 
 class SharePhotoController: UIViewController {
     
+    static let updateFeedNotificationName = NSNotification.Name(rawValue: "UpdateFeed")
     var selectedImage: UIImage? {
         didSet {
             self.imageView.image = selectedImage
@@ -102,9 +103,10 @@ class SharePhotoController: UIViewController {
                 print("failed to save post to DB: \(error.localizedDescription)")
                 return
             }
-            
             print("Successfully saved post to DB!")
             self.dismiss(animated: true, completion: nil)
+            
+            NotificationCenter.default.post(name: SharePhotoController.updateFeedNotificationName, object: nil)
         }
     }
 }
--- InstagramFIrebase/InstagramFIrebase/Home/HomeController.swift
+++ InstagramFIrebase/InstagramFIrebase/Home/HomeController.swift
@@ -18,8 +18,47 @@ class HomeController: UICollectionViewController {
         collectionView?.backgroundColor = .white
         collectionView?.register(HomePostCell.self, forCellWithReuseIdentifier: cellId)
         
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
+        collectionView?.refreshControl = refreshControl
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleUpdateFeed), name: SharePhotoController.updateFeedNotificationName, object: nil)
+        
         setupNavigataionItems()
+        fetchAllPosts()
+        
+    }
+    
+    @objc func handleUpdateFeed() {
+        handleRefresh()
+    }
+    
+    @objc func handleRefresh() {
+        print("Handling refresh...")
+        posts.removeAll(keepingCapacity: false)
+        fetchAllPosts()
+    }
+    
+    fileprivate func fetchAllPosts() {
         fetchPosts()
+        fetchFollowingUserIds()
+    }
+    
+    fileprivate func fetchFollowingUserIds() {
+        guard let loggedInUser = Auth.auth().currentUser?.uid else { return }
+        
+        Database.database().reference().child("users").child(loggedInUser).child("following").observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            guard let userIdsDictionary = snapshot.value as? [String: Any] else { return }
+            userIdsDictionary.forEach({ (key, value) in
+                Database.fetchUserWithUID(uid: key, completion: { (user) in
+                    self.fetchPostsWithUser(user: user)
+                })
+            })
+            
+        }) { (error) in
+            print("Failed to fetch following user IDs: \(error.localizedDescription)")
+        }
     }
     
     fileprivate func fetchPosts() {
@@ -34,6 +73,7 @@ class HomeController: UICollectionViewController {
 
         let ref = Database.database().reference().child("posts").child(user.uid)
         ref.observeSingleEvent(of: .value, with: { (snapshot) in
+            self.collectionView?.refreshControl?.endRefreshing()
             
             guard let dictionaries = snapshot.value as? [String: Any] else { return }
             
@@ -43,6 +83,10 @@ class HomeController: UICollectionViewController {
                 
                 let post = Post(user: user, dictionary: dictionary)
                 self.posts.append(post)
+            })
+            self.posts.sort(by: { (p1, p2) -> Bool in
+                return p1.creationDate.compare(p2.creationDate) == .orderedDescending
+                
             })
             self.collectionView?.reloadData()
             
@@ -63,7 +107,11 @@ extension HomeController: UICollectionViewDelegateFlowLayout {
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! HomePostCell
-        cell.post = posts[indexPath.item]
+        
+        if indexPath.item < posts.count {
+            cell.post = posts[indexPath.item]
+        }
+        
         return cell
     }
     

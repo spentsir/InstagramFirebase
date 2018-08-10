bcce9e17163b7108087a673b44c62016db176c17
Spencer Cawley <Spent_sir@Spencers-MacBook-Pro-2.local>
2018-08-10 15:42:11 -0600

    Added camera function to take & add photo to phones camera roll

---
+++ InstagramFIrebase/InstagramFIrebase/Camera/CameraController.swift
@@ -0,0 +1,116 @@
+//
+//  CameraController.swift
+//  InstagramFIrebase
+//
+//  Created by Spencer Cawley on 8/10/18.
+//  Copyright © 2018 Spencer Cawley. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraController: UIViewController, AVCapturePhotoCaptureDelegate {
+    
+    let output = AVCapturePhotoOutput()
+    
+    let dismissButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "right_arrow_shadow").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleDismiss), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func handleDismiss() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    let capturePhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "capture_photo").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleCapturePhoto), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func handleCapturePhoto() {
+        
+        let settings = AVCapturePhotoSettings()
+        guard let previewFormatType = settings.availablePreviewPhotoPixelFormatTypes.first else { return }
+        settings.previewPhotoFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewFormatType]
+        
+        output.capturePhoto(with: settings, delegate: self)
+        
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photoSampleBuffer: CMSampleBuffer?, previewPhoto previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+        
+        let imageData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer!, previewPhotoSampleBuffer: previewPhotoSampleBuffer)
+        
+        let previewImage = UIImage(data: imageData!)
+        
+        let containerView = PreviewPhotoContainerView()
+        containerView.previewImageView.image = previewImage
+        view.addSubview(containerView)
+        containerView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+//        let previewImageView = UIImageView(image: previewImage)
+//        view.addSubview(previewImageView)
+//        previewImageView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCaptureSession()
+        setupHUD()
+    }
+    
+    fileprivate func setupHUD() {
+        view.addSubview(capturePhotoButton)
+        capturePhotoButton.anchor(top: nil, left: nil, bottom: view.bottomAnchor, right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 24, paddingRight: 0, width: 80, height: 80)
+        capturePhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        view.addSubview(dismissButton)
+        dismissButton.anchor(top: view.topAnchor, left: nil, bottom: nil, right: view.rightAnchor, paddingTop: 12, paddingLeft: 0, paddingBottom: 0, paddingRight: 12, width: 50, height: 50)
+    }
+    
+    fileprivate func setupCaptureSession() {
+        let captureSession = AVCaptureSession()
+        
+        // 1. Setup inputs
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            if captureSession.canAddInput(input) {
+               captureSession.addInput(input)
+            }
+        } catch let error {
+            print("Could not setup camera input: \(error.localizedDescription)")
+        }
+ 
+        // 2. Setup outputs
+        if captureSession.canAddOutput(output) {
+          captureSession.addOutput(output)
+        }
+        
+        // 3. Setup output preview
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.frame
+        view.layer.addSublayer(previewLayer)
+    
+        // executes camera
+        captureSession.startRunning()
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ InstagramFIrebase/InstagramFIrebase/Camera/PreviewPhotoContainerView.swift
@@ -0,0 +1,107 @@
+//
+//  PreviewPhotoContainerView.swift
+//  InstagramFIrebase
+//
+//  Created by Spencer Cawley on 8/10/18.
+//  Copyright © 2018 Spencer Cawley. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class PreviewPhotoContainerView: UIView {
+    
+    let previewImageView: UIImageView = {
+        let iv = UIImageView()
+        return iv
+    }()
+    
+    let cancelButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "cancel_shadow").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleCancel), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func handleCancel() {
+        self.removeFromSuperview()
+    }
+    
+    let saveButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "save_shadow").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleSave), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func handleSave() {
+        
+        guard let previewImage = previewImageView.image else { return }
+        
+        let library = PHPhotoLibrary.shared()
+        library.performChanges({
+            
+            PHAssetChangeRequest.creationRequestForAsset(from: previewImage)
+            
+        }) { (success, error) in
+            if let error = error {
+                print("failed to save image to photo library: \(error.localizedDescription)")
+                return
+            }
+            print("successfully saved image to photo library")
+            
+            DispatchQueue.main.async {
+                
+                let savedLabel = UILabel()
+                savedLabel.text = "Saved Successfully"
+                savedLabel.font = UIFont.boldSystemFont(ofSize: 18)
+                savedLabel.textColor = .white
+                savedLabel.numberOfLines = 0
+                savedLabel.backgroundColor = UIColor(white: 0, alpha: 0.3)
+                
+                savedLabel.frame = CGRect(x: 0, y: 0, width: 150, height: 80)
+                savedLabel.textAlignment = .center
+                savedLabel.center = self.center
+                
+                self.addSubview(savedLabel)
+                
+                savedLabel.layer.transform = CATransform3DMakeScale(0, 0, 0)
+                
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                    
+                    savedLabel.layer.transform = CATransform3DMakeScale(1, 1, 1)
+                    
+                }, completion: { (completed) in
+                    // completed
+                    
+                    UIView.animate(withDuration: 0.5, delay: 0.75, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                        
+                        savedLabel.layer.transform = CATransform3DMakeScale(0.1, 0.1, 0.1)
+                        savedLabel.alpha = 0
+                        
+                    }, completion: { (_) in
+                        savedLabel.removeFromSuperview()
+                    })
+                })
+            }
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .yellow
+        
+        addSubview(previewImageView)
+        previewImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        addSubview(cancelButton)
+        cancelButton.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 12, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 50, height: 50)
+        
+        addSubview(saveButton)
+        saveButton.anchor(top: nil, left: leftAnchor, bottom: bottomAnchor, right: nil, paddingTop: 0, paddingLeft: 24, paddingBottom: 24, paddingRight: 0, width: 50, height: 50)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
